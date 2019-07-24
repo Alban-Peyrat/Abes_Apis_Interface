2ceadac579b1a06c2b0f5a355a7bf3c6676047fe
louxfaure <louxfaure@gmail.com>
2019-07-24 12:09:18 +0200

    Documentation

--- AbesXml.py
+++ AbesXml.py
@@ -7,10 +7,18 @@ import xml.etree.ElementTree as ET
 from mail import mail
 from logs import logs
 
+"""
+    AbesXml
+    =======
+    A set of function wich handle data returned by service 'Sudoc in Xml' 
+    http://documentation.abes.fr/sudoc/manuels/administration/aidewebservices/index.html#SudocMarcXML
+    On init take a PPN (sudoc identifier) in argument
+    ex : https://www.sudoc.fr/178565946.xml   
+"""
 
-##Permet de travailler avec l'api du SUDOC qui retourne une notice en xmL
 
 class AbesXml(object):
+
     def __init__(self,ppn,service='AbesXml'):
         self.endpoint = "https://www.sudoc.fr"
         self.service = service
@@ -22,23 +30,38 @@ class AbesXml(object):
             r.raise_for_status()  
         except requests.exceptions.HTTPError:
             raise HTTPError(r,self.service)
-        self.notice = r.content.decode('utf-8')
+        self.record = r.content.decode('utf-8')
 
     @property
-    #Retourne la totalite de la notice
-    def get_notice(self):
-        # print(self.notice)
-        return self.notice
-    #Pour un RCR donnée retourne la liste des Etats de collection formatés (955$$r du SUDOC)
-    def get_etat_col(self,rcr):
+    
+    def get_record(self):
+        """
+        Return the entire record
+        
+        Returns:
+            string -- the record in unimarc_xml 
+        """
+        return self.record
+    
+    def get_textual_holdings(self,rcr):
+        """
+        For a given library return all the text holdings present in the record (955$$r in unimarc SUDOC)
+        
+        Arguments:
+            rcr {string} -- the libray's id in Sudoc
+        
+        Returns:
+            list -- a list of text holdings
+        """
         root = ET.fromstring(self.notice)
-        etatsColl = []
-        for champ in root.findall(".//datafield[@tag='955']"):
-            item = champ.find("subfield[@code='5']").text
-            if item[:9] == rcr and champ.find("subfield[@code='r']") is not None:
-                etatColl = champ.find("subfield[@code='r']").text
-                etatsColl.append(etatColl)
-        return etatsColl
+        textual_holdings = []
+        for field in root.findall(".//datafield[@tag='955']"):
+            item = field.find("subfield[@code='5']").text
+            if item[:9] == rcr and field.find("subfield[@code='r']") is not None:
+                textual_holdings.append(field.find("subfield[@code='r']").text)
+        return textual_holdings
+
+
 #Gestion des erreurs
 class HTTPError(Exception):
 
